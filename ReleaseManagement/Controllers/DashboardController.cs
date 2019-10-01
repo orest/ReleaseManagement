7f5ffc96e8a490c5e2cd6f6ee4295f5ac46caf9f
Orest Win8 <orest.khrapko@cardworks.com>
2019-10-01 16:58:23 -0400

    add DashboardController

---
+++ ReleaseManagement/Controllers/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ReleaseManagement.Data.Context;
+
+namespace ReleaseManagement.Controllers {
+    public class DashboardController : ApiController {
+
+
+        public IHttpActionResult Get() {
+            using (var db = new ReleaseManagementContext()) {
+                var releaseCutoffDay = DateTime.Now.AddDays(-5);
+                var releases = db.Releases.Include(p => p.Client).Include(p => p.ReleasePlatforms)
+                    .Include(p => p.Features)
+                    .Include(p => p.WorkItems)
+                    .Where(p => p.ReleaseDate > releaseCutoffDay)
+                    .OrderByDescending(p => p.QaStartDate).ToList();
+                return Ok(releases);
+            }
+
+        }
+    }
+}
