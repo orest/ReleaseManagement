c48e8ffd3840403c8c345f134370469dd15e493c
Orest Win8 <orest.khrapko@cardworks.com>
2019-10-16 15:24:40 -0400

    filter out completed for dashboard

--- ReleaseManagement/Controllers/DashboardController.cs
+++ ReleaseManagement/Controllers/DashboardController.cs
@@ -14,10 +14,12 @@ namespace ReleaseManagement.Controllers {
         public IHttpActionResult Get() {
             using (var db = new ReleaseManagementContext()) {
                 var releaseCutoffDay = DateTime.Now.AddDays(-5);
+                
                 var releases = db.Releases.Include(p => p.Client).Include(p => p.ReleasePlatforms)
                     .Include(p => p.Features)
                     .Include(p => p.WorkItems)
-                    .Where(p => p.ReleaseDate > releaseCutoffDay)
+                    .Where(p => p.StatusCode != "completed")
+                    //.Where(p => p.ReleaseDate > releaseCutoffDay)
                     .OrderBy(p => p.ReleaseDate).ToList();
                 return Ok(releases);
             }
